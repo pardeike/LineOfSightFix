2c269404b4aa9a1851e1286dd03ecc091fc67c19
Andreas Pardeike <andreas@pardeike.net>
2021-11-25 02:56:40 +0100

    Add project files.

---
+++ README.md
@@ -0,0 +1,18 @@
+## RimWorld LineOfSightFix Mod
+
+### About
+
+This mod fixes the aiming towards pawns where vanilla targets cells around that pawn but the shot actually goes to the position of the pawn.
+
+Powered by [Harmony Patch Library](https://github.com/pardeike/Harmony)
+
+### License
+
+Free. As in free beer. Copy, learn and be respectful.
+
+### Contact
+
+Andreas Pardeike  
+Email: andreas@pardeike.net  
+Steam: pardeike  
+Twitter: @pardeike
---
+++ Source/Main.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace LineOfSightFix
+{
+	[StaticConstructorOnStartup]
+	public class Main
+	{
+		static Main()
+		{
+			var harmony = new Harmony("net.pardeike.harmony.LineOfSightFix");
+			harmony.PatchAll();
+		}
+	}
+
+	[HarmonyPatch(typeof(AttackTargetFinder), nameof(AttackTargetFinder.CanSee))]
+	public class AttackTargetFinder_CanSee_Patch
+	{
+		public static void CalcShootableCellsOfFixed(List<IntVec3> outCells, Thing t)
+		{
+			if (t is Pawn)
+			{
+				outCells.Clear();
+				outCells.Add(t.Position);
+				return;
+			}
+			else
+				ShootLeanUtility.CalcShootableCellsOf(outCells, t);
+		}
+
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var from = SymbolExtensions.GetMethodInfo(() => ShootLeanUtility.CalcShootableCellsOf(null, null));
+			var to = SymbolExtensions.GetMethodInfo(() => CalcShootableCellsOfFixed(null, null));
+			return instructions.MethodReplacer(from, to);
+		}
+	}
+}
